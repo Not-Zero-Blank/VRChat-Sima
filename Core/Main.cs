87cf966b120437842698bd4821ba49733b067838
⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ <JanTV720@gmail.com>
2022-04-07 00:16:23 +0200

    Module Core

---
+++ Core/Main.cs
@@ -0,0 +1,118 @@
+﻿using MelonLoader;
+using QuickMenuLib;
+using System;
+using Core;
+
+public class Main : MelonLoader.MelonMod
+{
+    public const string Version = "1.0.0.0";
+    public const string Name = "Template";
+    public const string Author = "Blank";
+    public override void OnApplicationStart()
+    {
+        AppDomain.CurrentDomain.UnhandledException += HandleException;
+        Console.WriteLine("Hello");
+        Core.ModuleManager.RegisterModules();
+        foreach (ModuleManager.Module a in ModuleManager.Modules) a.OnApplicationStart();
+    }
+    public override void OnApplicationLateStart()
+    {
+        foreach (ModuleManager.Module a in ModuleManager.Modules) a.OnApplicationLateStart();
+    }
+    public override void OnApplicationQuit()
+    {
+        foreach (ModuleManager.Module a in ModuleManager.Modules) a.OnApplicationQuit();
+    }
+    public override void OnFixedUpdate()
+    {
+        foreach (ModuleManager.Module a in ModuleManager.Modules) a.OnFixedUpdate();
+    }
+    public override void OnGUI()
+    {
+        foreach (ModuleManager.Module a in ModuleManager.Modules) a.OnGUI();
+    }
+    public override void OnLateUpdate()
+    {
+        foreach (ModuleManager.Module a in ModuleManager.Modules) a.OnLateUpdate();
+    }
+    public override void OnPreferencesLoaded()
+    {
+        foreach (ModuleManager.Module a in ModuleManager.Modules) a.OnPreferencesLoaded();
+    }
+    public override void OnPreferencesLoaded(string filepath)
+    {
+        foreach (ModuleManager.Module a in ModuleManager.Modules) a.OnPreferencesLoaded(filepath);
+    }
+    public override void OnPreferencesSaved()
+    {
+        foreach (ModuleManager.Module a in ModuleManager.Modules) a.OnPreferencesSaved();
+    }
+    public override void OnPreferencesSaved(string filepath)
+    {
+        foreach (ModuleManager.Module a in ModuleManager.Modules) a.OnPreferencesLoaded(filepath);
+    }
+    public override void OnPreSupportModule()
+    {
+        foreach (ModuleManager.Module a in ModuleManager.Modules) a.OnPreSupportModule();
+    }
+    public override void OnSceneWasInitialized(int buildIndex, string sceneName)
+    {
+        foreach (ModuleManager.Module a in ModuleManager.Modules) a.OnSceneWasInitialized(buildIndex, sceneName);
+    }
+    public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+    {
+        foreach (ModuleManager.Module a in ModuleManager.Modules) a.OnSceneWasLoaded(buildIndex, sceneName);
+    }
+    public override void OnSceneWasUnloaded(int buildIndex, string sceneName)
+    {
+        foreach (ModuleManager.Module a in ModuleManager.Modules) a.OnSceneWasUnloaded(buildIndex, sceneName);
+    }
+    public override void OnUpdate()
+    {
+        foreach (ModuleManager.Module a in ModuleManager.Modules) a.OnUpdate();
+    }
+    private void HandleException(object sender, UnhandledExceptionEventArgs e)
+    {
+        Logs.Error(e.ExceptionObject.ToString());
+    }
+}
+public class Menu : ModMenu
+{
+    public override string MenuName => Main.Name;
+    public Menu()
+    {
+        Logo = null; // You can put a sprite here and QuickMenuLib will automatically add it.
+    }
+    public override void OnQuickMenuInitialized()
+    {
+        var category = MyModMenu.AddMenuCategory("TestModMenuCat");
+        category.AddButton("Test", "This is a test using QuickMenuLib!", () =>
+        {
+            MelonLogger.Msg("Test Button!");
+        });
+
+        MyModMenu.AddSlider("FunnySlider", "Test", (num) => MelonLogger.Msg(num));
+    }
+    public override void OnWingMenuLeftInitialized()
+    {
+        MyLeftWingMenu.AddButton("Test", "Test using QuickMenuLib!", () =>
+        {
+            MelonLogger.Msg("Test Wing Button!");
+        });
+    }
+    public override void OnWingMenuRightInitialized()
+    {
+        MyRightWingMenu.AddButton("Test", "Test using QuickMenuLib!", () =>
+        {
+            MelonLogger.Msg("Test Wing Button!");
+        });
+    }
+    public override void OnTargetMenuInitialized()
+    {
+        MyTargetMenu.AddButton("Test", "Test using QuickMenuLib!", () =>
+        {
+            MelonLogger.Msg(SelectedUser.prop_String_0);
+        });
+    }
+}
+
---
+++ Core/ModuleManager.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public interface IModule
+    {
+        /// <summary>
+        /// Runs when the Module gets Enabled
+        /// </summary>
+        void OnEnable();
+        /// <summary>
+        /// Runs when the Module gets Disabled
+        /// </summary>
+        void OnDisable();
+        /// <summary>
+        /// Runs when a Scene has Loaded and is passed the Scene's Build Index and Name.
+        /// </summary>
+        /// <param name="buildIndex"></param>
+        /// <param name="sceneName"></param>
+        void OnSceneWasLoaded(int buildIndex, string sceneName);
+        /// <summary>
+        /// Runs when a Scene has Initialized and is passed the Scene's Build Index and Name.
+        /// </summary>
+        /// <param name="buildIndex"></param>
+        /// <param name="sceneName"></param>
+        void OnSceneWasInitialized(int buildIndex, string sceneName);
+        /// <summary>
+        /// Runs when a Scene has Unloaded and is passed the Scene's Build Index and Name.
+        /// </summary>
+        /// <param name="buildIndex"></param>
+        /// <param name="sceneName"></param>
+        void OnSceneWasUnloaded(int buildIndex, string sceneName);
+        /// <summary>
+        /// Can run multiple times per frame. Mostly used for Physics.
+        /// </summary>
+        void OnFixedUpdate();
+        void OnPreSupportModule();
+        void OnApplicationStart();
+        void OnApplicationLateStart();
+        void OnUpdate();
+        void OnLateUpdate();
+        void OnGUI();
+        void OnApplicationQuit();
+        void OnPreferencesSaved();
+        void OnPreferencesSaved(string filepath);
+        void OnPreferencesLoaded();
+        void OnPreferencesLoaded(string filepath);
+    }
+    public static class ModuleManager
+    {
+        public class Module
+        {
+            public Module(Type module)
+            {
+                try
+                {
+                    type = module;
+                    Logs.Text("Initalizing " + type.Name + "from " + AssemblyName);
+                    instance = Activator.CreateInstance(module);
+                }
+                catch (Exception e)
+                {
+                    Logs.Error($"Coudnt Load {module.Name} Reason: {e}");
+                    return;
+                }
+                OnEnableMethod = module.GetMethod(nameof(OnEnable));
+                OnDisableMethod = module.GetMethod(nameof(OnDisable));
+                OnFixedUpdateMethod = module.GetMethod(nameof(OnFixedUpdate));
+                OnSceneWasInitializedMethod = module.GetMethod(nameof(OnSceneWasInitialized));
+                OnSceneWasLoadedMethod = module.GetMethod(nameof(OnSceneWasLoaded));
+                OnSceneWasUnloadedMethod = module.GetMethod(nameof(OnSceneWasUnloaded));
+                OnPreSupportModuleMethod = module.GetMethod(nameof(OnPreSupportModule));
+                OnApplicationStartMethod = module.GetMethod(nameof(OnApplicationStart));
+                OnApplicationLateStartMethod = module.GetMethod(nameof(OnApplicationLateStart));
+                OnUpdateMethod = module.GetMethod(nameof(OnUpdate));
+                OnLateUpdateMethod = module.GetMethod(nameof(OnLateUpdate));
+                OnGUIMethod = module.GetMethod(nameof(OnGUI));
+                OnApplicationQuitMethod = module.GetMethod(nameof(OnApplicationQuit));
+                OnPreferencesSavedMethod = module.GetMethod(nameof(OnPreferencesSaved), new Type[0]);
+                OnPreferencesSaved2Method = module.GetMethod(nameof(OnPreferencesSaved), new Type[] { typeof(System.String) });
+                OnPreferencesLoadedMethod = module.GetMethod(nameof(OnPreferencesLoaded), new Type[0]);
+                OnPreferencesLoaded2Method = module.GetMethod(nameof(OnPreferencesLoaded), new Type[] { typeof(System.String) });
+                Modules.Add(this);
+            }
+            public Type type;
+            public string ModuleName => type.Name;
+            public string AssemblyName => type.Assembly.GetName().Name;
+            object instance;
+            bool enabled = true;
+            #region MethodInfos
+            MethodInfo OnEnableMethod;
+            MethodInfo OnDisableMethod;
+            MethodInfo OnFixedUpdateMethod;
+            MethodInfo OnSceneWasInitializedMethod;
+            MethodInfo OnSceneWasLoadedMethod;
+            MethodInfo OnSceneWasUnloadedMethod;
+            MethodInfo OnPreSupportModuleMethod;
+            MethodInfo OnApplicationStartMethod;
+            MethodInfo OnApplicationLateStartMethod;
+            MethodInfo OnUpdateMethod;
+            MethodInfo OnLateUpdateMethod;
+            MethodInfo OnGUIMethod;
+            MethodInfo OnApplicationQuitMethod;
+            MethodInfo OnPreferencesSavedMethod;
+            MethodInfo OnPreferencesSaved2Method;
+            MethodInfo OnPreferencesLoadedMethod;
+            MethodInfo OnPreferencesLoaded2Method;
+            #endregion
+            #region Methods
+            public void OnEnable()
+            {
+                try
+                {
+                    OnEnableMethod.Invoke(instance, null);
+                    enabled = true;
+                }
+                catch (NotImplementedException a) 
+                {
+                    Logs.Error($"Cant Enable {ModuleName} Its not implemented!");
+                    enabled = false;
+                }
+                catch (Exception e)
+                {
+                    Logs.Error($"Failed Enabling {ModuleName} Reason: " + e);
+                    enabled = false;
+                }
+            }
+            public void OnDisable()
+            {
+                try
+                {
+                    OnDisableMethod.Invoke(instance, null);
+                    enabled = false;
+                }
+                catch (NotImplementedException a)
+                {
+                    Logs.Error($"Cant Enable {ModuleName} Its not implemented!");
+                    enabled = false;
+                }
+                catch (Exception e)
+                {
+                    Logs.Error($"Failed Disabling {ModuleName} Reason: " + e);
+                    enabled = false;
+                }
+            }
+            public void OnSceneWasLoaded(int buildIndex, string sceneName)
+            {
+                if (!enabled) return;
+                try
+                {
+                    OnSceneWasLoadedMethod.Invoke(instance, new object[] { buildIndex, sceneName });
+                }
+                catch (NotImplementedException a) { }
+                catch (Exception e) { Logs.Error($"Failed OnSceneWasLoaded {ModuleName} Reason: {e}"); }
+            }
+            public void OnSceneWasInitialized(int buildIndex, string sceneName)
+            {
+                if (!enabled) return;
+                try
+                {
+                    OnSceneWasInitializedMethod.Invoke(instance, new object[] { buildIndex, sceneName });
+                }
+                catch (NotImplementedException a) { }
+                catch (Exception e) { Logs.Error($"Failed OnSceneWasInitialized {ModuleName} Reason: {e}"); }
+            }
+            public void OnSceneWasUnloaded(int buildIndex, string sceneName)
+            {
+                if (!enabled) return;
+                try
+                {
+                    OnSceneWasUnloadedMethod.Invoke(instance, new object[] { buildIndex, sceneName });
+                }
+                catch (NotImplementedException a) { }
+                catch (Exception e) { Logs.Error($"Failed OnSceneWasUnloaded {ModuleName} Reason: {e}"); }
+            }
+            public void OnFixedUpdate()
+            {
+                if (!enabled) return;
+                try
+                {
+                    OnFixedUpdateMethod.Invoke(instance, null);
+                }
+                catch (NotImplementedException a) { }
+                catch (Exception e) { Logs.Error($"Failed OnFixedUpdate {ModuleName} Reason: {e}"); }
+            }
+            public void OnPreSupportModule()
+            {
+                if (!enabled) return;
+                try
+                {
+                    OnPreSupportModuleMethod.Invoke(instance, null);
+                }
+                catch (NotImplementedException a) { }
+                catch (Exception e) { Logs.Error($"Failed OnPreSupportModule {ModuleName} Reason: {e}"); }
+            }
+            public void OnApplicationStart()
+            {
+                if (!enabled) return;
+                try
+                {
+                    OnApplicationStartMethod.Invoke(instance, null);
+                }
+                catch (NotImplementedException a) { }
+                catch (Exception e) { Logs.Error($"Failed OnApplicationStart {ModuleName} Reason: {e}"); }
+            }
+            public void OnApplicationLateStart()
+            {
+                if (!enabled) return;
+                try
+                {
+                    OnApplicationLateStartMethod.Invoke(instance, null);
+                }
+                catch (NotImplementedException a) { }
+                catch (Exception e) { Logs.Error($"Failed OnApplicationLateStart {ModuleName} Reason: {e}"); }
+            }
+            public void OnUpdate()
+            {
+                if (!enabled) return;
+                try
+                {
+                    OnUpdateMethod.Invoke(instance, null);
+                }
+                catch (NotImplementedException a) { }
+                catch (Exception e) { Logs.Error($"Failed OnUpdate {ModuleName} Reason: {e}"); }
+            }
+            public void OnLateUpdate()
+            {
+                if (!enabled) return;
+                try
+                {
+                    OnLateUpdateMethod.Invoke(instance, null);
+                }
+                catch (NotImplementedException a) { }
+                catch (Exception e) { Logs.Error($"Failed OnLateUpdate {ModuleName} Reason: {e}"); }
+            }
+            public void OnGUI()
+            {
+                if (!enabled) return;
+                try
+                {
+                    OnGUIMethod.Invoke(instance, null);
+                }
+                catch (NotImplementedException a) { }
+                catch (Exception e) { Logs.Error($"Failed OnGUI {ModuleName} Reason: {e}"); }
+            }
+            public void OnApplicationQuit()
+            {
+                if (!enabled) return;
+                try
+                {
+                    OnApplicationQuitMethod.Invoke(instance, null);
+                }
+                catch (NotImplementedException a) { }
+                catch (Exception e) { Logs.Error($"Failed OnApplicationQuit {ModuleName} Reason: {e}"); }
+            }
+            public void OnPreferencesSaved()
+            {
+                if (!enabled) return;
+                try
+                {
+                    OnPreferencesSavedMethod.Invoke(instance, null);
+                }
+                catch (NotImplementedException a) { }
+                catch (Exception e) { Logs.Error($"Failed OnPreferencesSaved {ModuleName} Reason: {e}"); }
+            }
+            public void OnPreferencesSaved(string filepath)
+            {
+                if (!enabled) return;
+                try
+                {
+                    OnPreferencesSaved2Method.Invoke(instance, new object[] { filepath });
+                }
+                catch (NotImplementedException a) { }
+                catch (Exception e) { Logs.Error($"Failed OnPreferencesSaved {ModuleName} Reason: {e}"); }
+            }
+            public void OnPreferencesLoaded()
+            {
+                if (!enabled) return;
+                try
+                {
+                    OnPreferencesLoadedMethod.Invoke(instance, null);
+                }
+                catch (NotImplementedException a) { }
+                catch (Exception e) { Logs.Error($"Failed OnPreferencesLoaded {ModuleName} Reason: {e}"); }
+            }
+            public void OnPreferencesLoaded(string filepath)
+            {
+                if (!enabled) return;
+                try
+                {
+                    OnPreferencesLoaded2Method.Invoke(instance, new object[] { filepath });
+                }
+                catch (NotImplementedException a) { }
+                catch (Exception e) { Logs.Error($"Failed OnPreferencesLoaded {ModuleName} Reason: {e}"); }
+            }
+            #endregion
+        }
+        public static List<Module> Modules = new List<Module>();
+        public static void RegisterModules()
+        {
+    var interfacetype = typeof(IModule);
+    var types = AppDomain.CurrentDomain.GetAssemblies()
+        .SelectMany(s => s.GetTypes())
+        .Where(p => interfacetype.IsAssignableFrom(p) && !p.IsInterface);
+    Logs.Warning($"Found [{types.Count()}] Modules");
+    foreach (Type a in types) new Module(a);
+}
+    }
+}
